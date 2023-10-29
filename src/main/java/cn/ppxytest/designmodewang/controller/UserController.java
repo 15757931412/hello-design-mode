72ece629b365a98f13c41f8849e38c5804b56a43
水自强 <773723844@qq.com>
2023-10-29 12:03:16 +0800

    init

--- src/main/java/cn/ppxytest/designmodewang/controller/UserController.java
+++ src/main/java/cn/ppxytest/designmodewang/controller/UserController.java
@@ -1,6 +1,7 @@
 package cn.ppxytest.designmodewang.controller;
 
 import cn.ppxytest.designmodewang.pojo.UserInfo;
+import cn.ppxytest.designmodewang.service.Login3rdAdapter;
 import cn.ppxytest.designmodewang.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,8 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private Login3rdAdapter login3rdAdapter;
 
     @PostMapping("/login")
     public String login(String account, String password) {
@@ -19,5 +22,10 @@ public class UserController {
     @PostMapping("/register")
     public String register(@RequestBody UserInfo userInfo) {
         return userService.register(userInfo);
+    }
+
+    @GetMapping("/gitee")
+    public String gitee(String code,String state){
+        return login3rdAdapter.loginByGitee(code, state);
     }
 }
--- src/main/java/cn/ppxytest/designmodewang/service/Login3rdAdapter.java
+++ src/main/java/cn/ppxytest/designmodewang/service/Login3rdAdapter.java
@@ -1,14 +1,53 @@
 package cn.ppxytest.designmodewang.service;
 
+import cn.ppxytest.designmodewang.pojo.UserInfo;
+import cn.ppxytest.designmodewang.util.HttpClientUtils;
+import com.alibaba.fastjson2.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 
-@Component
-public class Login3rdAdapter extends UserService implements Login3rdTarget{
+import java.util.Date;
 
+@Component
+public class Login3rdAdapter extends UserService implements Login3rdTarget {
+    @Value("${gitee.state}")
+    private String giteeState;
+    @Value("${gitee.token.url}")
+    private String giteeTokenUrl;
+    @Value("${gitee.user.url}")
+    private String giteeUserUrl;
+    @Value("${gitee.user.prefix}")
+    private String giteeUserPrefix;
 
     @Override
     public String loginByGitee(String code, String state) {
-        return null;
+        if (!giteeState.equals(state)) {
+            throw new UnsupportedOperationException("Invalid state!");
+        }
+        String tokenUrl = giteeTokenUrl.concat(code);
+        JSONObject tokenResponse = HttpClientUtils.execute(tokenUrl, HttpMethod.POST);
+        String token = String.valueOf(tokenResponse.get("access_token"));
+        String userUrl = giteeUserUrl.concat(token);
+        JSONObject userInfoResponse = HttpClientUtils.execute(userUrl, HttpMethod.GET);
+
+        String userName = giteeUserPrefix.concat(String.valueOf(userInfoResponse.get("name")));
+        String password = userName;
+
+        return autoRegister3rdAndLogin(userName, password);
+    }
+
+    private String autoRegister3rdAndLogin(String userName, String password) {
+        if (super.checkUserExists(userName)) {
+            return super.login(userName, password);
+        }
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUserName(userName);
+        userInfo.setUserPassword(password);
+        userInfo.setCreateDate(new Date());
+
+        super.register(userInfo);
+        return super.login(userName, password);
     }
 
     @Override
