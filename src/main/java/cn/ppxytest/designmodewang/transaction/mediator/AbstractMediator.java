f10c9021bb45a9cb7858df1e628d15861d3b716b
szq <1346181034@qq.com>
2023-11-11 15:45:57 +0800

    feat: 设计模式完结

--- src/main/java/cn/ppxytest/designmodewang/transaction/mediator/AbstractMediator.java
+++ src/main/java/cn/ppxytest/designmodewang/transaction/mediator/AbstractMediator.java
@@ -6,6 +6,6 @@ import cn.ppxytest.designmodewang.transaction.colleague.AbstractCustomer;
  * @author: 水自强
  * @create-date: 2023/11/11 14:37
  */
-public class AbstractMediator {
+public abstract class AbstractMediator {
     public abstract void messageTransfer(String orderId, String targetCustomer, AbstractCustomer customer, String payResult);
 }
